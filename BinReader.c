2cdf551bcd1ffe83930180d8ea927a4c678eb853
Xuri Li <dlshle@hotmail.com>
2019-02-25 22:15:17 -0600

    first commit

---
+++ BinReader.c
@@ -0,0 +1,130 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void open_file(FILE *fp, char* file_addr, char mode);
+void read_binary_file(FILE *fp, char mode);
+void print_content(char *buffer, long size, char mode);
+void print_element(char *buffer, unsigned int index, char mode);
+
+void handle_error(char *msg)
+{
+	printf("%s\n",msg);
+	exit(1);
+}
+
+int main(int argc, char* argv[])
+{
+	if(argc<3)
+		handle_error("ERROR: incomplete arguments!");
+	FILE *fp;
+	open_file(fp, argv[1], argv[2][0]);
+	return 0;
+}
+
+void open_file(FILE *fp, char* file_addr, char mode)
+{
+	fp = fopen(file_addr, "rb");
+	if(fp == NULL)
+		handle_error("ERROR: Unable to open file!\n");
+	printf("File: %s\n", file_addr);
+	read_binary_file(fp, mode);	
+}
+
+void read_binary_file(FILE *fp, char mode)
+{
+	long file_size;
+	size_t result;
+	fseek(fp, 0, SEEK_END);
+	file_size = ftell(fp);
+	rewind(fp);
+	
+	char *buffer = (char*)malloc(sizeof(char)*file_size);
+	if(buffer==NULL)
+	{
+		free(buffer);
+		printf("ERROR Info:\nfile_size=%lu\n",file_size);
+		handle_error("ERROR: Insufficient heap space for allocation!");
+	}
+	result = fread(buffer, 1, file_size, fp);
+	if(result!=file_size)
+	{
+		free(buffer);
+		printf("ERROR Info:\nfile_size=%lu\nresult=%lu\n",file_size, result);
+		handle_error("ERROR: File reading error!");
+	}
+	printf("Size = %lu bytes\n", file_size);
+	print_content(buffer, file_size, mode);	
+	printf("\n");
+	fclose(fp);
+	free(buffer);
+}
+
+void print_content(char *buffer, long size, char mode)
+{
+	for(unsigned int i=0;i<size;i++)
+	{
+		print_element(buffer, i, mode);
+	}
+}
+
+void print_element(char *buffer, unsigned int index, char mode)
+{
+	switch(mode)
+	{
+		case 'x':
+			if(index%16==0)
+			{
+				if(index%256==0&&index>0)
+					printf("\n--------------------------------------------------------------------------");
+				printf("\n%05x: ", index);
+			}
+			printf("%02x ", (int)(*(unsigned char*)(&buffer[index])));
+			break;
+		case 'c':
+			printf("%c", buffer[index]);
+			break;
+		case 'd':
+			if(index%16==0)
+				printf("\n%05x: ", index);
+			printf("%03d ", (int)(*(unsigned char*)(&buffer[index])));
+			break;
+		case 'C':
+			if(index%16==0)
+			{
+				if(index)
+				{
+					printf(" |");
+					for(unsigned int i=index-16;i<index;i++)
+					{
+						printf("%c", (buffer[i]<32||buffer[i]>126?'.':buffer[i]));
+					}
+					printf("|");
+				}
+				if(index%256==0&&index>0)
+					printf("\n--------------------------------------------------------------------------");
+				printf("\n%05x: ", index);
+			}
+			printf("%02x ", (int)(*(unsigned char*)(&buffer[index])));
+			
+			break;
+		case 'D':
+			if(index%16==0)
+			{
+				if(index)
+				{
+					printf(" |");
+					for(unsigned int i=index-16;i<index;i++)
+					{
+						printf("%03d ", (int)(*(unsigned char*)(&buffer[index])));
+					}
+					printf("|");
+				}
+				if(index%256==0&&index>0)
+					printf("\n--------------------------------------------------------------------------------------------------------------------------");
+				printf("\n%05x: ", index);
+			}
+			printf("%02x ", (int)(*(unsigned char*)(&buffer[index])));
+			
+			break;
+	}
+}
